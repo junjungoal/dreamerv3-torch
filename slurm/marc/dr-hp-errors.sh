0d8796ea1f8d00bd41384648e4f56b4bf7e48637
Marc Rigter <marcrigter@gmail.com>
2023-11-17 09:08:45 +0000

    further increase dreamer init

--- slurm/marc/dr-hp-errors.sh
+++ slurm/marc/dr-hp-errors.sh
@@ -23,7 +23,7 @@ conda activate diffusion
 
 SEED=${SLURM_ARRAY_TASK_ID}
 TASK=Hopper-v3
-GROUP=dreamer-errors-final_datasets_nov12-fix
+GROUP=dreamer-errors-final_datasets_nov12-increase-init
 DATASET=/data/engs-a2i/pemb5572/diffusion/datasets/final_datasets_nov12/final-rl-runs-lowtrainratio_seed1_Hopper
 LOAD_STEP=1000000
 H=310
